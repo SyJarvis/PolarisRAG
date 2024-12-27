eab955ee10b88fcf83ac784aeb84de25cb43179c
SyJarvis <1755115828@qq.com>
2024-12-27 18:05:12 +0800

    feat:新增MilvusDB配置的常量

--- polarisrag/const.py
+++ polarisrag/const.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+
+MilvusDB_CONF = {
+    "db_file": "milvus_data.db",
+    "collection_name": "default",
+    "embedding_dim": 10,
+    "search_params": {
+        "metric_type": "IP",
+        "params": {}
+    },
+    "output_fields": ["text"],
+    "limit": 3
+}
