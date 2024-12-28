0c80f71c925e8658398dfbc176f6f5d2a862fd8e
SyJarvis <1755115828@qq.com>
2024-12-28 11:40:11 +0800

    fix:修复了智谱从环境变量添加api_key

--- polarisrag/llm.py
+++ polarisrag/llm.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from typing import List, Dict
 
 from langchain_zhipu import ChatZhipuAI
@@ -21,18 +22,24 @@ class ZhipuLLM(BaseLLM):
     智谱AI的封装
     """
 
-    def __init__(self, api_key: str,
+    def __init__(self, api_key: str = None,
                  base_url = None,
                  model: str = "glm-4-flash",
                  temperature: float = 0.8,
                  is_memory: bool = False,
                  system_prompt: str = None):
         super().__init__(model, api_key, base_url)
+        if api_key:
+            os.environ["ZHIPUAI_API_KEY"] = api_key
+        else:
+            api_key = os.environ.get("ZHIPUAI_API_KEY") if "ZHIPUAI_API_KEY" in os.environ else api_key
+            if api_key is None:
+                raise ValueError("ZHIPUAI_API_KEY is None")
         self.api_key = api_key
         self.model = model
         self.base_url = base_url
         self.temperature = temperature
-        self.is_memory = is_memory
+        self.is_memory = True if is_memory == "True" or is_memory == True else False
         self.client = ChatZhipuAI(
             model=self.model,
             temperature=self.temperature,
